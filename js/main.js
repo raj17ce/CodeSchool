bcd10afb043a9fac78e9e370781d5e26dd6bbe31
Raj Patel <raj17ce@gmail.com>
2023-02-12 16:13:00 +0530

    Added FAQs Section on HomePage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Window Scroll fixed NavBar
+
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+// Show/Hide FAQs
+
+const faqs = document.querySelectorAll('.faq-card');
+
+faqs.forEach(x => {
+    x.addEventListener('click', () => {
+        x.classList.toggle('open');
+
+        //change icon
+        const icon = x.querySelector(".faq-icon i");
+
+        if(icon.className == "uil uil-plus") {
+            icon.className = "uil uil-minus"
+        }
+        else{
+            icon.className = "uil uil-plus"
+        }
+    })
 })
